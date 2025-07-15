1a43169da76eeafc0d0cc2d68fb9782d31bb8f62
Hiddenpanda003 <67002030+Hiddenpanda003@users.noreply.github.com>
2025-07-15 19:30:09 +0200

    Update db.php

--- fahrzeugseite/db.php
+++ fahrzeugseite/db.php
@@ -4,7 +4,11 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=fahrzeug_db;charset=utf8', 'root', '');
+    $pdo = new PDO(
+        'mysql:host=sql308.infinityfree.com;dbname=if0_39478841_fahrzeuge;charset=utf8',
+        'if0_39478841', // Benutzername
+        'FQIxfCF93l' // <== Passwort hier eintragen
+    );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     http_response_code(500);
